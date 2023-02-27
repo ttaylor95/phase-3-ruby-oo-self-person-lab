6376a8aa41af79b457d990fb67042c012afa79bd
Teanna <teannamtaylor95@gmail.com>
2023-02-26 18:26:06 -0800

    commit tree 302b53597206f06d8b4558329c1c4c54a543b93a

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,26 @@
 # your code goes here
+require 'pry'
+
+# everytime you craete a person, they have to have a name
+class Person 
+    #will only let you read the name and not set it
+    #you need a method to access the below variables per instance/person
+    #getter method
+    attr_reader :name , :bank_account
+    def initialize(name)
+        @name = name 
+        # setter method
+        # instance variables with default values
+        # bc they will be initialized with every instance
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+end 
+
+
+
+
+p = Person.new("Noeul")
+
+binding.pry 
